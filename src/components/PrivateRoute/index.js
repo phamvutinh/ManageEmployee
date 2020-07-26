3cbe201f1255c5fe66e1c74c662e245835b30f72
Pham Tinh <phamvu.tinh99@gmail.com>
2020-07-26 13:05:09 +0700

    [Update] modified Private route

--- src/components/PrivateRoute/index.js
+++ src/components/PrivateRoute/index.js
@@ -1,21 +1,28 @@
 import React from "react";
 import PropTypes from "prop-types";
+import decode from "jwt-decode";
 import { Route, Redirect } from "react-router-dom";
 
-const PrivateRoute = ({ component: Component, ...rest }) => (
-  <Route
-    {...rest}
-    render={(props) =>
-      sessionStorage.getItem("userData") ? (
-        <Component {...props} />
-      ) : (
-        <Redirect
-          to={{ pathname: "/login", state: { from: props.location } }}
-        />
-      )
-    }
-  />
-);
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const expToken =
+    sessionStorage.getItem("userData") &&
+    decode(sessionStorage.getItem("userData"));
+  const validAuth = expToken && Date.now() <= expToken.exp * 1000;
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        validAuth ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{ pathname: "/login", state: { from: props.location } }}
+          />
+        )
+      }
+    />
+  );
+};
 
 PrivateRoute.propTypes = {
   component: PropTypes.elementType,
